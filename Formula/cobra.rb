a9e9c60dec87f6ddb97f54790770562a4d429630
Martin Ring <martin.ring@dfki.de>
2016-06-20 16:25:50 +0200

    cobra v1.0.5

--- Formula/cobra.rb
+++ Formula/cobra.rb
@@ -1,7 +1,7 @@
 class Cobra < Formula
   desc "Proof and Code Presentation Framework"
   homepage "http://www.flatmap.net/cobra"
-  url "https://github.com/flatmap/cobra/releases/download/version-1.0.4/cobra-1.0.4.zip"
+  url "https://github.com/flatmap/cobra/releases/download/version-1.0.5/cobra-1.0.5.zip"
   sha256 "987354e447247884020a957f5bc2c32ec6f1fd63d6e6a12a927ba69dba9e9ec7"
 
   bottle :unneeded
